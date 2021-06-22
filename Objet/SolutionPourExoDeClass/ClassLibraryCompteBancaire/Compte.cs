4b00a1e4b0cd582422079b34f7be990c7c907dd9
darkbanor <fakegamer333@gmail.com>
2021-06-22 11:57:18 +0200

    CpmtBancaire_fini_220621
    Nettoyage du code et amelioration de la presentation en mode console + teste sur plusieurs methode = ok

--- Objet/SolutionPourExoDeClass/ClassLibraryCompteBancaire/Compte.cs
+++ Objet/SolutionPourExoDeClass/ClassLibraryCompteBancaire/Compte.cs
@@ -15,14 +15,12 @@ namespace ClassLibraryCompteBancaire
 
         public Compte() //constructeur par defaut
         {
+            //pour exo le constructeur = zero (=commenté)
             //decouvertAutorise = -150;
             //nom = "Olivier";
             //numero = 123456;
             //solde = 1000;
-        }       
-        
-        public int Solde
-        { get => solde; }
+        }
 
         public Compte(string _nom, int _numero, int _solde, int _decouvertAutorise)
         {
@@ -31,9 +29,10 @@ namespace ClassLibraryCompteBancaire
             solde = _solde;
             decouvertAutorise = _decouvertAutorise;
         }
+
         public void Crediter(int _montant)
         {
-            solde += _montant;
+            solde += _montant; // rajoute un montant au solde
         }
 
         public bool Debiter(int _montant)
@@ -41,7 +40,8 @@ namespace ClassLibraryCompteBancaire
             bool ok = false;
             int nouveauSolde = solde - _montant;
 
-            if (nouveauSolde >= decouvertAutorise)
+            //on debite seulement dans les limites de l'autorisation de decouvert
+            if (nouveauSolde >= decouvertAutorise)  
             {
                 solde -= _montant;
                 ok = true;
@@ -53,37 +53,42 @@ namespace ClassLibraryCompteBancaire
 
         public bool Superieur(Compte _autreCompte)
         {
-            bool ok = false;            
+            bool ok = false;
 
-            if (this.solde > -_autreCompte.solde)                
-                {
-                   
-                }
+            //compare si le solde courant est superieur au nouveau solde 
+            if (this.solde > _autreCompte.solde)               
+            {
+                ok = true;
+            }
 
             return ok;
-        }
-
-        public override string ToString()
-        {
-            return " nom = " + nom + " numero de compte = "+ numero  +
-                " solde = " + solde + " Decouvert Autorise = " + decouvertAutorise;
         }
 
         public bool Transferer(int _montantTransfert, Compte _compteDestination)
         {
             bool ok = false;
 
-            if (this.Debiter(_montantTransfert))
+            //on regarde si le compte courant peut etre debiter
+            if (this.Debiter(_montantTransfert)) 
             {
-                //compte à crediter                 
+                //si on peut debiter on credite le nouveau compte                 
                 _compteDestination.Crediter(_montantTransfert);
                 ok = true;
 
             }
 
-            return ok;            
+            return ok;
 
         }
+
+        // sert a afficher les données
+        public override string ToString()
+        {
+            return " nom = " + nom + " numero de compte = "+ numero  +
+                " solde = " + solde + " Decouvert Autorise = " + decouvertAutorise;
+        }
+
+        
     }
 }
 
--- Objet/SolutionPourExoDeClass/ConsoleAppCompteBancaire/Program.cs
+++ Objet/SolutionPourExoDeClass/ConsoleAppCompteBancaire/Program.cs
@@ -11,15 +11,39 @@ namespace ConsoleAppCompteBancaire
     {
         static void Main(string[] args)
         {
-            Compte c = new Compte();
-            Compte c1 = new Compte("toto", 654321, -500, -100);
+            //  exo Poo
 
+            Compte c = new Compte();
+            Compte c1 = new Compte("toto", 654321, 1000, -500); // nom, num de compte, solde, debit autoriser
+
+            //affiche resultat conforme à exo 1.
+            Console.WriteLine("------------resultats de la creation de comptes");
             Console.WriteLine(c.ToString());
             Console.WriteLine(c1.ToString());
+            Console.WriteLine();
 
-            Compte b = new Compte("laurent", 546254, 2000, -500);
-            b.Crediter(100);
+            //exercice 2. methode crediter et debiter
+            Console.WriteLine("------------resultats exo pour crediter et debiter");
+            Compte b = new Compte("laurent", 545454, 2000, -500);
+
+            //afficher compte avant credit
+            Console.WriteLine("avant crédit de 100");
             Console.WriteLine(b);
+            b.Crediter(100);
+            Console.WriteLine();
+
+            //afficher compte apres debit
+            Console.WriteLine("aprés crédit de 100");
+            Console.WriteLine(b);
+            Console.WriteLine();
+
+            //afficher compte avant debit de 100000
+            Console.WriteLine("avant débit de 100000");
+            Console.WriteLine(b);
+            Console.WriteLine();
+
+            //afficher compte apres debit de 100000
+            Console.WriteLine("aprés débit de 100000");
             bool ok = b.Debiter(100000);
             Console.WriteLine(b.ToString());
             if (ok)
@@ -30,43 +54,57 @@ namespace ConsoleAppCompteBancaire
             {
                 Console.WriteLine("Débit pas Réussi !");
             }
+            Console.WriteLine();
 
-            // get Solde
-            //c1.Solde;
 
-            //pour test de débit
-            bool ok1 = b.Debiter(123);
-            Console.WriteLine(b.ToString());
-            if (ok1)
-            {
-                Console.WriteLine("Débit Réussi !");
-            }
-            else
-            {
-                Console.WriteLine("Débit pas Réussi !");
-            }
-            //
+            //transfert d'une sommes exo2
+            Console.WriteLine("------------resultats exo pour transfert d'une somme");
+            Compte c2 = new Compte("toto", 12345, 1000, -500); // c2 car c1 est utiliser
+            Compte c3 = new Compte("titi", 45657, 2000, -1000);
+            Compte c4 = new Compte("toto", 12345, 1000, -500);
+            Compte c5 = new Compte("titi", 45657, 2000, -1000);
 
-            //transfert d'une sommes
-            Compte c2 = new Compte("tata", 12345, 2000, -500);
-            Compte c3 = new Compte("titi", 45657, 1000, -1000);
-            
-            c2.Transferer(3300, c3);
-
+            //compte avant transfert 
+            Console.WriteLine("compte avant tout transfert");
             Console.WriteLine(c2.ToString());
             Console.WriteLine(c3.ToString());
+            Console.WriteLine();
 
-            if (c2.Superieur(c3))
+            //aprés transfert 1300
+            c2.Transferer(1300, c3); // c2=c1 c3=c2 dans exo
+
+            Console.WriteLine("aprés transfert de 1300");
+            Console.WriteLine(c2.ToString());
+            Console.WriteLine(c3.ToString());
+            Console.WriteLine();
+
+            // transfert d'une somme 2ime partie
+            c4.Transferer(3300, c5); // c4=c1 c5=c2 dans exo
+
+            Console.WriteLine("aprés transfert de 3300 ");
+            Console.WriteLine(c4.ToString());
+            Console.WriteLine(c5.ToString());
+            Console.WriteLine();
+
+            //comparer 2 comptes
+            Console.WriteLine("------------resultat exo compte courant est il superieur à :");
+            //compte avant transfert 
+            Console.WriteLine("compte :");
+            Console.WriteLine(c4.ToString());
+            Console.WriteLine("est compare à :");
+            Console.WriteLine(c5.ToString());
+            Console.WriteLine();
+
+            if (c4.Superieur(c5)) // dans exo c4=c1 c5=c2
             {
-                Console.WriteLine("superieur");
+                Console.WriteLine("résultat = superieur");
             }
             else
             {
-                Console.WriteLine("inferieur");
+                Console.WriteLine("résultat = inferieur");
             }
-            
-            
-            
+
+
             Console.ReadKey();
         }
     }
