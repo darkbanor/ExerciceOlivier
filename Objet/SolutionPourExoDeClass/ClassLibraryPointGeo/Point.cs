fa2ea46b985477bc72128c89ff45d98843d91fc6
darkbanor <fakegamer333@gmail.com>
2021-06-23 11:46:56 +0200

    maj_PointGeo_230621
    maj pour inclure exo point

---
+++ Objet/SolutionPourExoDeClass/ClassLibraryPointGeo/Point.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPointGeo
+{
+    public class Point
+    {
+        private float x;
+        private float y;
+
+        public Point() //constructeur par defaut
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(float _absisseX, float _ordonneeY)
+        {
+            x = _absisseX;
+            y = _ordonneeY;
+        }
+
+        public Point(Point _point)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Deplacer(float _nouveauX, float _nouveauY)
+        {
+            x = _nouveauX;
+            y = _nouveauY;
+        }
+
+        public void Permuter()
+        {
+            if (y > 0 && x > 0) //bissectile y0 et x0
+            {
+                float temp = x;
+                x = y;
+                y = temp;
+            }
+            
+        }
+
+        public void SymetrieAxeAbsisse()
+        {
+            y = -y;
+        }
+
+        public void SymetrieAxeOrdonnee()
+        {
+            x = -x;
+        }
+
+        public void SymetrieAxeOrigine()
+        {
+            y = -y;
+            x = -x;
+        }
+
+        // sert a afficher les données
+        public override string ToString()
+        {
+            return " x = " + x + " y = " + y;
+        }
+    }
+}
---
+++ Objet/SolutionPourExoDeClass/ConsoleAppPointGeo/Program.cs
@@ -0,0 +1,51 @@
+﻿using ClassLibraryPointGeo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPointGeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cree premier coordonnee par defaut
+            Point ncoor = new Point();
+
+            //afficher ncoor
+            Console.WriteLine("Absisse X,  Ordonnee Y");
+            Console.WriteLine(ncoor.ToString());
+
+            //deplacer Point
+            ncoor.Deplacer(1, 2);
+
+            //afficher ncoor
+            Console.WriteLine("nouveau coordonnee : Absisse X,  Ordonnee Y");
+            Console.WriteLine(ncoor.ToString());
+
+            ncoor.SymetrieAxeOrigine();
+
+            Console.WriteLine("Symetrie axe Origine : ");
+            Console.WriteLine(ncoor.ToString());
+
+            ncoor.SymetrieAxeOrigine();
+            Console.WriteLine(ncoor.ToString());
+
+            ncoor.Permuter();
+            Console.WriteLine("Permutation  : ");
+            Console.WriteLine(ncoor.ToString());
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
